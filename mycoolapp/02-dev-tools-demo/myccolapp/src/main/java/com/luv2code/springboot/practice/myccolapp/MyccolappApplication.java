a4dbac5cdab4aae6d31ebd998d0088635ec8a3e2
Cade <cadeconner8@gmail.com>
2023-11-21 16:19:04 -0600

    first commit 01 and 02

---
+++ mycoolapp/02-dev-tools-demo/myccolapp/src/main/java/com/luv2code/springboot/practice/myccolapp/MyccolappApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.practice.myccolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyccolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyccolappApplication.class, args);
+	}
+
+}
