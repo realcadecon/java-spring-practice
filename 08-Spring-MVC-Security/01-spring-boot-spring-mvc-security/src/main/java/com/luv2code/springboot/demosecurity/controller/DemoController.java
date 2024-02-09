bc2af91ed9abf68a168d38f88420279207a4ece8
concad (non-WSL) <cadeconner8@gmail.com>
2024-02-09 08:34:26 -0600

    finish spring security

--- 08-Spring-MVC-Security/01-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 08-Spring-MVC-Security/01-spring-boot-spring-mvc-security/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -10,4 +10,14 @@ public class DemoController {
     public String showHome() {
         return "home";
     }
+
+    @GetMapping("/leaders")
+    public String showLeadersHome() {
+        return "leader-home";
+    }
+
+    @GetMapping("/systems")
+    public String showSystemsHome() {
+        return "system-home";
+    }
 }
