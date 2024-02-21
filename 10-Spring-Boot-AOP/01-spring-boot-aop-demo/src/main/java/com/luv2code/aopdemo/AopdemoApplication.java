9d4a98491520d0827e6e7a8c029fdd193d7f49b1
concad <cadeconner8@gmail.com>
2024-02-21 14:17:36 -0600

    start AOP section

---
+++ 10-Spring-Boot-AOP/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -0,0 +1,28 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(String[] args, AccountDAO accountDAO) {
+		return runner -> {
+			demoTheBeforeAdvice(accountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO) {
+		accountDAO.addAccount();
+	}
+
+
+}
